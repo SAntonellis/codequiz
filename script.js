231575de3df7c52c50b123f2cbe831d3f9a2325a
SophiaA <sophia.antonellis@gmail.com>
2021-07-27 00:51:30 -0400

    added some of the quiz questions

--- script.js
+++ script.js
@@ -18,3 +18,138 @@ myQuiz();
 
 // show results after pressing submit button:
 submitButton.addEventListener('click', quizResults);
+
+
+// quiz questions (SOURCED FROM W3schools.com):
+
+const quizQuestions = [ 
+
+//Q1
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: {
+       1: "<script>",
+       2: "<javascript>",
+       3: "<js>",
+       4: "<scripting>"
+    },
+    // answer section
+    correctAnswer: "1"
+  },
+
+//Q2
+  {
+    question: "What is the correct JavaScript syntax to change the content of the HTML element below?", "<p id="demo">This is a demonstration.</p>",
+    answer: {
+       1: "document.getElementByName("p").innerHTML = "Hello World!";",
+       2: "document.getElementById("demo").innerHTML = "Hello World!";  ",
+       3: "#demo.innerHTML = "Hello World!";",
+       4: "document.getElement("p").innerHTML = "Hello World!";"
+    },
+    // answer section
+    correctAnswer: "2"
+  },  
+  
+//Q3  
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: {
+      1: "",
+      2: "",
+      3: "",
+      4: ""
+    },
+    // answer section
+    correctAnswer: "1"
+  },  
+//Q4  
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: {
+      1: "",
+      2: "",
+      3: "",
+      4: ""
+    },
+    // answer section
+    correctAnswer: "1"
+  },
+
+//Q5  
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: {
+      1: "",
+      2: "",
+      3: "",
+      4: ""
+    },
+    // answer section
+    correctAnswer: "1"
+  },
+
+//Q6  
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: {
+      1: "",
+      2: "",
+      3: "",
+      4: ""
+    },
+    // answer section
+    correctAnswer: "1"
+  },
+
+//Q7 
+{
+  question: "Inside which HTML element do we put the JavaScript?",
+  answer: {
+    1: "",
+    2: "",
+    3: "",
+    4: ""
+  },
+  // answer section
+  correctAnswer: "1"
+},  
+
+//Q8
+{
+  question: "Inside which HTML element do we put the JavaScript?",
+  answer: {
+    1: "",
+    2: "",
+    3: "",
+    4: ""
+  },
+  // answer section
+  correctAnswer: "1"
+},
+
+//Q9  
+{
+  question: "Inside which HTML element do we put the JavaScript?",
+  answer: {
+    1: "",
+    2: "",
+    3: "",
+    4: ""
+  },
+  // answer section
+  correctAnswer: "1"
+},
+
+//Q10
+{
+  question: "Inside which HTML element do we put the JavaScript?",
+  answer: {
+    1: "",
+    2: "",
+    3: "",
+    4: ""
+  },
+  // answer section
+  correctAnswer: "1"
+}
+];
