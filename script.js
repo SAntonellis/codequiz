fb389fca0b540d7a2f35f7a9764632121cefa8ea
SophiaA <sophia.antonellis@gmail.com>
2021-07-26 21:06:10 -0400

    added functions for quiz, changed bg color to make sure CSS linked

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const quizContainer = document.getElementById('quiz');
 const submitButton = document.getElementById('submit')
 
 const resultsContainer = document.getElementById('results');
+
+
+
+// functions
+
+function myQuiz(){}
+
+function quizResults(){}
+
+
+// running myQuiz function first to display the quiz on page immediately
+myQuiz();
+
+// show results after pressing submit button:
+submitButton.addEventListener('click', quizResults);
