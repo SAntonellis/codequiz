021bde3f27af0bfad03cc25d7752aa249bbf5e8c
SophiaA <sophia.antonellis@gmail.com>
2021-07-31 20:14:20 -0400

    replaced css and html files with up to date code, restructured folders

---
+++ JS/questions.js
@@ -0,0 +1,60 @@
+//array
+
+let questions = [
+    {
+        numb: 1,
+        question: "Question 1 test?",
+        answer: "Answer 1 (correct)",
+        options: [
+            "Answer 1 (correct)",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Question 2",
+        answer: "Answer 1 (correct)",
+        options: [
+            "Answer 1 (correct)",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Question 3",
+        answer: "Answer 2 (correct)",
+        options: [
+            "Answer 1",
+            "Answer 2 (correct)",
+            "Answer 3",
+            "Answer 4",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Question 4",
+        answer: "Answer 3 (correct)",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3 (correct)",
+            "Answer 4",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Question 5",
+        answer: "Answer 4 (correct)",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4 (correct)",
+        ]
+    },
+];
+
